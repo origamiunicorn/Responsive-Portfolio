d6686183fc0af8e3c3efc37d1bcfb588d93ace77
R. V. Wright <rvwright@gmail.com>
2022-10-27 17:39:03 -0700

    Added in functional form using emailjs.com functionality. Added in abiilty to have form reset via js on submit, and to add a 'message sent' notice above the form on submit.

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,36 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+// email form for contact page follows:
+
+(function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('6YNc26NMSt6M_44MP');
+})();
+
+function fun() {
+    document.getElementsByName('user_name').value = "";
+    document.getElementsByName('user_email').value = "";
+    document.getElementsByName('message').value = "";
+};
+
+window.onload = function () {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+
+        form.reset();
+        document.getElementById('submess').innerHTML = "Your message has been sent!"
+    });
+};
